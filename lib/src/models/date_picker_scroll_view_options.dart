2c6e12f653a3771d32cf5f04a2635da1e7b892e7
SyedAhkam <smahkam57@gmail.com>
2022-08-11 18:53:18 +0530

    Add a new `all` constructor to scroll view options

--- lib/src/models/date_picker_scroll_view_options.dart
+++ lib/src/models/date_picker_scroll_view_options.dart
@@ -10,6 +10,15 @@ class DatePickerScrollViewOptions {
   final ScrollViewDetailOptions year;
   final ScrollViewDetailOptions month;
   final ScrollViewDetailOptions day;
+
+  // Applies the given [ScrollViewDetailOptions] to all three options ie. year, month and day.
+ static DatePickerScrollViewOptions all(ScrollViewDetailOptions value) {
+    return DatePickerScrollViewOptions(
+      year: value,
+      month: value,
+      day: value,
+    );
+  }
 }
 
 class ScrollViewDetailOptions {
