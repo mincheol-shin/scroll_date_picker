cca6beb308f0aadd32237c1618cce0b226e645ee
Mincheol Shin <mincheolnihs@gmail.com>
2024-03-19 14:34:58 +0900

    Revert "use intl for months translations (#35)"
    This reverts commit b34d5133bfbe06a682cf3e2e5b68a2fa12cf4b89.

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:intl/intl.dart';
 
 const String ko = 'ko';
 const String fr = 'fr';
@@ -18,21 +17,220 @@ const String tr = 'tr';
 
 extension LocaleExtension on Locale {
   List<String> get months {
-    return localizedMonths(this);
+    switch (languageCode) {
+      case zh:
+      case ko:
+        return intMonths;
+      case fr:
+        return frMonths;
+      case de:
+        return deMonths;
+      case vi:
+        return viMonths;
+      case id:
+        return idMonths;
+      case it:
+        return itMonths;
+      case th:
+        return thMonths;
+      case es:
+        return esMonth;
+      case nl:
+        return nlMonth;
+      case ar:
+        return arMonths;
+      case pt:
+        return ptMonths;
+      case tr:
+        return trMonths;
+      default:
+        return enMonths;
+    }
   }
 }
 
-List<String> localizedMonths(Locale loc) => [
-      DateFormat.MMMM(loc.languageCode).format(DateTime(2000, 01)),
-      DateFormat.MMMM(loc.languageCode).format(DateTime(2000, 02)),
-      DateFormat.MMMM(loc.languageCode).format(DateTime(2000, 03)),
-      DateFormat.MMMM(loc.languageCode).format(DateTime(2000, 04)),
-      DateFormat.MMMM(loc.languageCode).format(DateTime(2000, 05)),
-      DateFormat.MMMM(loc.languageCode).format(DateTime(2000, 06)),
-      DateFormat.MMMM(loc.languageCode).format(DateTime(2000, 07)),
-      DateFormat.MMMM(loc.languageCode).format(DateTime(2000, 08)),
-      DateFormat.MMMM(loc.languageCode).format(DateTime(2000, 09)),
-      DateFormat.MMMM(loc.languageCode).format(DateTime(2000, 10)),
-      DateFormat.MMMM(loc.languageCode).format(DateTime(2000, 11)),
-      DateFormat.MMMM(loc.languageCode).format(DateTime(2000, 12)),
-    ];
+const List<String> intMonths = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  '11',
+  '12',
+  '13',
+];
+const List<String> enMonths = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const List<String> frMonths = [
+  'Janvier',
+  'Février',
+  'Mars',
+  'Avril',
+  'Mai',
+  'Juin',
+  'Juillet',
+  'Août',
+  'Septembre',
+  'Octobre',
+  'Novembre',
+  'Décembre',
+];
+const List<String> deMonths = [
+  'Januar',
+  'Februar',
+  'März',
+  'April',
+  'Mai',
+  'Juni',
+  'Juli',
+  'August',
+  'September',
+  'Oktober',
+  'November',
+  'Dezember',
+];
+const List<String> viMonths = [
+  'Tháng 1',
+  'Tháng 2',
+  'Tháng 3',
+  'Tháng 4',
+  'Tháng 5',
+  'Tháng 6',
+  'Tháng 7',
+  'Tháng 8',
+  'Tháng 9',
+  'Tháng 10',
+  'Tháng 11',
+  'Tháng 12',
+];
+const List<String> idMonths = [
+  'Januari',
+  'Februari',
+  'Maret',
+  'April',
+  'Mei',
+  'Juni',
+  'Juli',
+  'Agustus',
+  'September',
+  'Oktober',
+  'November',
+  'Desember',
+];
+const List<String> itMonths = [
+  'Gennaio',
+  'Febbraio',
+  'Marzo',
+  'Aprile',
+  'Maggio',
+  'Giugno',
+  'Luglio',
+  'Agosto',
+  'Settembre',
+  'Ottobre',
+  'Novembre',
+  'Dicembre',
+];
+const List<String> thMonths = [
+  'มกราคม',
+  'กุมภาพันธ์',
+  'มีนาคม',
+  'เมษายน',
+  'พฤษภาคม',
+  'มิถุนายน',
+  'กรกฎาคม',
+  'สิงหาคม',
+  'กันยายน',
+  'ตุลาคม',
+  'พฤศจิกายน',
+  'ธันวาคม',
+];
+const List<String> esMonth = [
+  'Enero',
+  'Febrero',
+  'Marzo',
+  'Abril',
+  'Mayo',
+  'Junio',
+  'Julio',
+  'Agosto',
+  'Septiembre',
+  'Octubre',
+  'Noviembre',
+  'Diciembre',
+];
+const List<String> nlMonth = [
+  'Januari',
+  'Februari',
+  'Maart',
+  'April',
+  'Mei',
+  'Juni',
+  'Juli',
+  'Augustus',
+  'September',
+  'Oktober',
+  'November',
+  'December',
+];
+const List<String> arMonths = [
+  'يناير',
+  'فبراير',
+  'مارس',
+  'أبريل',
+  'مايو',
+  'يونيو',
+  'يوليو',
+  'أغسطس',
+  'سبتمبر',
+  'أكتوبر',
+  'نوفمبر',
+  'ديسمبر',
+];
+
+const List<String> ptMonths = [
+  'janeiro',
+  'fevereiro',
+  'março',
+  'abril',
+  'maio',
+  'junho',
+  'julho',
+  'agosto',
+  'setembro',
+  'outubro',
+  'novembro',
+  'dezembro',
+];
+
+const List<String> trMonths = [
+  'Ocak',
+  'Şubat',
+  'Mart',
+  'Nisan',
+  'Mayıs',
+  'Haziran',
+  'Temmuz',
+  'Ağustos',
+  'Eylül',
+  'Ekim',
+  'Kasım',
+  'Aralık',
+];
